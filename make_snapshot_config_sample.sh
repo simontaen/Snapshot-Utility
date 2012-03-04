c0f594049d078233c1885c8e4369bf4220bfaf76
Simon Tännler <simon@mba-simon.fritz.box>
2012-03-04 08:52:35 +0100

    Config Sample
    Added config sample file

---
+++ make_snapshot_config_sample.sh
@@ -0,0 +1,39 @@
+# THIS SCRIPT MUST BE SOURCED!
+
+######################## Sample OSX Version ##################################
+
+##############################################################################
+# system commands used by this script
+#
+
+ID=/usr/bin/id;
+ECHO=/bin/echo;
+GETOPT=getopts;
+RM=/bin/rm;
+MV=/bin/mv;
+CP=/bin/cp;
+TOUCH=/usr/bin/touch;
+DATE=/bin/date;
+RSYNC=/usr/bin/rsync;
+FIND=/usr/bin/find;
+UNAME=/usr/bin/uname;
+
+##############################################################################
+# ssh configuration
+#
+
+SSH=/usr/bin/ssh;
+SERVER=myServer.example.com;
+USER=root;
+SSHPORT=22;
+SSHKEY=/location/of/rsync/keys;
+
+##############################################################################
+# system commands subject to the remote machine
+#
+
+R_RSYNC=/bin/rsync;
+R_ECHO=/bin/echo;
+R_MV=/bin/mv;
+R_RM=/bin/rm;
+R_TOUCH=/bin/touch;
